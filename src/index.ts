211b37127581e0b5763c152133fbbd702c7f7a29
Bariq Mbani <bariq.m70@gmail.com>
2024-03-02 09:41:57 +0700

    refactor: key characters,length && depends on mongo

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,36 @@
 import { connect } from "./db/db-connection";
 import { UniqueKey } from "./db/schema/UniqueKey";
-import { generateAndUseUniqueKey, generateUniqueKeyJob } from "./key-generation";
+import { generateAndUseUniqueKey, generateUniqueKeyJob } from "./key-generator";
 import RabbitMQ from "./mq/RabbitMQ";
-import http from "http";
+import dotenv from "dotenv";
 
-connect().then(() => {
-  console.log("Connected to MongoDB");
-});
+dotenv.config();
 
-generateUniqueKeyJob.start();
+const { SHORTEN_URL_REQ_EXCHANGE, SHORTEN_URL_RES_EXCHANGE } = process.env;
 
-const app = async () => {
+export const app = async () => {
+  try {
+    await connect();
+    generateUniqueKeyJob.start();
+    console.info("Connected to MongoDB");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+    process.exit(1);
+  }
+
   const mq = new RabbitMQ();
   await mq.connect();
 
-  await mq.consume("get-unique-key", async (msg) => {
+  await mq.consume(SHORTEN_URL_REQ_EXCHANGE || "req", async (msg) => {
     console.info(`Received message from client: ${msg}`);
     const data = await UniqueKey.findOneAndUpdate({ used: false }, { used: true });
     const key = data ? data.key : await generateAndUseUniqueKey();
     console.info(`Sending key to client: ${key}`);
-    await mq.sendToQueue("unique-key", {
+    await mq.sendToQueue(SHORTEN_URL_RES_EXCHANGE || "res", {
       key,
       client: JSON.parse(msg),
     });
   });
 };
 
-const HTTP_PORT = process.env.PORT || 4001;
-http.createServer().listen(HTTP_PORT, () => {
-  app();
-  console.log(`Server is running on port ${HTTP_PORT}`);
-});
+app();
---
+++ src/key-generator.ts
@@ -0,0 +1,70 @@
+import { CronJob } from "cron";
+import { UniqueKey } from "./db/schema/UniqueKey";
+
+const MAX_KEY_TO_GENERATE = parseInt(process.env.MAX_KEY_TO_GENERATE!) || 10;
+const KEY_CHARACTERS = process.env.KEY_CHARACTERS || "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const KEY_LENGTH = parseInt(process.env.KEY_LENGTH!) || 6;
+
+export const generateRandomKey = (length: number = KEY_LENGTH) => {
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * KEY_CHARACTERS.length);
+    const character = KEY_CHARACTERS[randomIndex];
+    result += character;
+  }
+  return result;
+};
+
+export const generateAndUseUniqueKey = async () => {
+  let exist = true;
+  let key = "";
+
+  while (exist) {
+    key = generateRandomKey();
+    exist = Boolean(await UniqueKey.exists({ key }));
+  }
+
+  try {
+    await UniqueKey.create({ key, used: true });
+    return key;
+  } catch (error: any) {
+    if (error.code === 11000 || (error.name === "MongoServerError" && error.message.includes("duplicate key"))) {
+      generateAndUseUniqueKey();
+    } else {
+      // Handle other errors
+      console.error("Error:", error);
+    }
+  }
+};
+
+export const generateUniqueKeyJob = new CronJob(
+  process.env.KEY_GENERATION_CRON_SCHEDULE || "*/15 * * * *", // 15/30/45/60 minutes by default
+  async () => {
+    const unusedKeyCount = await UniqueKey.countDocuments({ used: false });
+
+    const keyToGenerate = MAX_KEY_TO_GENERATE - unusedKeyCount;
+
+    if (keyToGenerate <= 0) {
+      return;
+    }
+
+    console.info(`Generating ${keyToGenerate} keys...`);
+
+    for (let i = 0; i < keyToGenerate; i++) {
+      const key = generateRandomKey();
+      try {
+        await UniqueKey.create({ key });
+        console.info(`Key ${key} generated!`);
+      } catch (error: any) {
+        if (error.code === 11000 || (error.name === "MongoServerError" && error.message.includes("duplicate key"))) {
+          console.info(`Key ${key} already exists!`);
+        } else {
+          // Handle other errors
+          console.error("Error:", error);
+        }
+      }
+    }
+
+    console.info(`Done generating keys!`);
+  }
+);
